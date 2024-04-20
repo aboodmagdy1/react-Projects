9039cb5b1eb3f0c483877d10cedb3f84af805261
Abood Magdy <c.aboodmagdy@gmail.com>
2024-04-20 19:27:50 +0200

    starter files

---
+++ tours/src/Loading.js
@@ -0,0 +1,9 @@
+function loading() {
+  return (
+    <div className="loading">
+      <h1>loading...</h1>
+    </div>
+  );
+}
+
+export default loading;
---
+++ tours/src/Tours.js
@@ -0,0 +1,9 @@
+function Tours() {
+  return (
+    <div>
+      <h2>Our Tours</h2>
+    </div>
+  );
+}
+
+export default Tours;
